f79b09efc29cf8c3485f4a593754cdfc6ed49ea6
Matthew Caswell <mattpcaswell@gmail.com>
2013-01-12 12:34:56 -0500

    ADDING THE PROGECT

---
+++ flying game/Classes/flying_gameAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  flying_gameAppDelegate.h
+//  flying game
+//
+//  Created by Matthew Caswell on 1/12/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface flying_gameAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ flying game/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  flying game
+//
+//  Created by Matthew Caswell on 1/12/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
